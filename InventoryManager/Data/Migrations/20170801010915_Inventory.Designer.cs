c0f26010d7fb449d03617fd63589871e079412eb
Sean Kerry <spkerry@live.com>
2017-07-31 20:11:38 -0500

    Inventory Migration

--- InventoryManager/Data/Migrations/20170801010915_Inventory.Designer.cs
+++ InventoryManager/Data/Migrations/20170801010915_Inventory.Designer.cs
@@ -8,9 +8,10 @@ using InventoryManager.Data;
 namespace InventoryManager.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170801010915_Inventory")]
+    partial class Inventory
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.2")
---
+++ InventoryManager/Data/Migrations/20170801010915_Inventory.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace InventoryManager.Data.Migrations
+{
+    public partial class Inventory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Catagories",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Catagories", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vendors",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Cost = table.Column<float>(nullable: false),
+                    DateAdded = table.Column<DateTime>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Unit = table.Column<string>(nullable: true),
+                    Upc = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vendors", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Items",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CategoryID = table.Column<int>(nullable: false),
+                    Cost = table.Column<float>(nullable: false),
+                    DateAdded = table.Column<DateTime>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Quantity = table.Column<float>(nullable: false),
+                    Upc = table.Column<string>(nullable: true),
+                    VendorID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Items", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Items_Catagories_CategoryID",
+                        column: x => x.CategoryID,
+                        principalTable: "Catagories",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Items_Vendors_VendorID",
+                        column: x => x.VendorID,
+                        principalTable: "Vendors",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Inventories",
+                columns: table => new
+                {
+                    InventoryID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Current = table.Column<bool>(nullable: false),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Discriminator = table.Column<string>(nullable: false),
+                    ItemID = table.Column<int>(nullable: true),
+                    OrderID = table.Column<int>(nullable: false),
+                    ID = table.Column<int>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    OrderDate = table.Column<DateTime>(nullable: true),
+                    ReceivedDate = table.Column<DateTime>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Inventories", x => x.InventoryID);
+                    table.ForeignKey(
+                        name: "FK_Inventories_Items_ItemID",
+                        column: x => x.ItemID,
+                        principalTable: "Items",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Inventories_Inventories_OrderID",
+                        column: x => x.OrderID,
+                        principalTable: "Inventories",
+                        principalColumn: "InventoryID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Inventories_ItemID",
+                table: "Inventories",
+                column: "ItemID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Inventories_OrderID",
+                table: "Inventories",
+                column: "OrderID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Items_CategoryID",
+                table: "Items",
+                column: "CategoryID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Items_VendorID",
+                table: "Items",
+                column: "VendorID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Inventories");
+
+            migrationBuilder.DropTable(
+                name: "Items");
+
+            migrationBuilder.DropTable(
+                name: "Catagories");
+
+            migrationBuilder.DropTable(
+                name: "Vendors");
+        }
+    }
+}
