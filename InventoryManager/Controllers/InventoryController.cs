de6ea86478f3bf84e9a627d63e6a2e1ab0d7aa77
Sean Kerry <spkerry@live.com>
2017-08-10 11:52:53 -0500

    Added index/add item views and additemVM; modifed invcontroler, layout, & AddCatVM

--- InventoryManager/Controllers/InventoryController.cs
+++ InventoryManager/Controllers/InventoryController.cs
@@ -12,7 +12,55 @@ namespace InventoryManager.Controllers
 {
     public class InventoryController : Controller
     {
-        
+        private ApplicationDbContext context;
+
+        public InventoryController(ApplicationDbContext dbContext)
+        {
+            this.context = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            IList<InventoryItem> items = context.Items.Include(i => i.Category).ToList();
+
+            return View(items);
+        }
+
+        public IActionResult Add()
+        {
+            AddInventoryItemViewModel addInventoryItem = new AddInventoryItemViewModel(context.Categories.ToList());
+
+            return View(addInventoryItem);
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddInventoryItemViewModel addInventoryItemViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                InventoryCategory newInventoryCategory = context.Categories.Single(c => c.ID == addInventoryItemViewModel.CategoryID);
+
+                InventoryItem newItem = new InventoryItem
+                {
+                    Name = addInventoryItemViewModel.Name,
+                    Upc = addInventoryItemViewModel.Upc,
+                    Cost = addInventoryItemViewModel.Cost,
+                    Quantity = addInventoryItemViewModel.Quantity,
+                    DateAdded = DateTime.Now.Date,
+                    Category = newInventoryCategory
+                };
+
+                context.Items.Add(newItem);
+                context.SaveChanges();
+
+                return Redirect("/Inventory/Index");
+            }
+            else
+            {
+                AddInventoryItemViewModel addInventoryItemViewModel1 = new AddInventoryItemViewModel(context.Categories.ToList());
+                return View(addInventoryItemViewModel1);
+            }
+        }
     }
     
 }
---
+++ InventoryManager/ViewModels/AddInventoryItemViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using InventoryManager.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace InventoryManager.ViewModels
+{
+    public class AddInventoryItemViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        public string Upc { get; set; }
+
+        public float Cost { get; set; }
+
+        public float Quantity { get; set; }
+
+        [Required]
+        [Display(Name = "Category")]
+        public int CategoryID { get; set; }
+
+        public List<SelectListItem> Categories { get; set; }
+
+        public AddInventoryItemViewModel() { }
+
+        public AddInventoryItemViewModel(IEnumerable<InventoryCategory> categories)
+        {
+            Categories = new List<SelectListItem>();
+
+            foreach (var category in categories)
+            {
+                Categories.Add(new SelectListItem
+                {
+                    Value = category.ID.ToString(),
+                    Text = category.Name
+                });
+            }
+        }
+    }
+}
